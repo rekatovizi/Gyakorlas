75cce992fa5cd3f2a067dc3fbfba980b375584f2
TóviziRéka <TviziRka@RRF16E-001722>
2026-09-16 09:08:36 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gyakorlas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
